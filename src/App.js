4355d1de6d008ec95a2acc9d8f7fafa2dcaf4313
Sathipriya13 <138269491+Sathipriya13@users.noreply.github.com>
2023-08-07 12:49:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import List from "./Component/List";
+import Table from "./Component/Table";
+import Webpage from "./Component/Webpage";
+import "./App.css"
+function App(){
+  return (
+    <div>
+    <List/>
+    <Table/>
+    <Webpage/>
+    </div>
+  );
+}
+export default App;
---
+++ src/Component/Webpage.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function webpage(){
+    return(
+    <div className="App">
+      <h1>Sarah's Favourite animal</h1>
+      <img src ="https://media.istockphoto.com/id/1469203149/photo/bull-moose-in-the-snake-river-wyoming-during-the-fall-rut.jpg?s=1024x1024&w=is&k=20&c=2_nKDXYSJCSVyUlOnDBXGV9dcz1NyRSIslMCacmhQXY=" alt='moose' width="400px"></img>
+      <div className="animals">
+      
+
+       <p>My name is Sathi priya,and my favourite animal is the moose.</p>
+       <br></br>They can be yp to 6.5 feet tall <br></br>Surprisingly,they're really good at swimming!<br></br>If you want to learn more about the moose ,check out <a href="https://www.kxnet.com/wp-content/uploads/sites/16/2019/04/moose_1555613752170_83157028_ver1.0.jpg?strip=1">its Wikipedia page.</a> 
+    </div>
+       <h2>Moose are awesome because.... </h2>
+     
+   <ol type="1">
+       <li>They can swim really well</li>
+       <li>They can make reallyfunny noises</li>
+       <li>They can lose their nostrils</li>
+   
+       
+   </ol>
+   <h2>If I were a moose,I would....</h2>
+   <ul>
+       <li>Eat lots of lettuce
+        </li><li>Carry little animals on my antlers</li><li>Swim to Alaska </li>
+   </ul>
+   <h2>Moose Video</h2>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/RTuRbbgQg3c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><div className="App">
+</div>
+</div>
+    );
+}
